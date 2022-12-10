92d8d9a1109c3120440d0e8c76ea75209f80ecd7
a.shein <alexander.shein1993@gmail.com>
2022-12-10 23:14:47 +0700

    fourth step

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/a-shein/fullstack-javascript-project-44/workflows/hexlet-check/badge.svg)](https://github.com/a-shein/fullstack-javascript-project-44/actions)
+<a href="https://codeclimate.com/github/a-shein/fullstack-javascript-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/0075c5cd564e1c363f66/maintainability" /></a>
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
-function getUserName()
-{
-    console.log("Welcome to the Brain Games!");
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+function getUserName() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 }
 
 export default getUserName;
